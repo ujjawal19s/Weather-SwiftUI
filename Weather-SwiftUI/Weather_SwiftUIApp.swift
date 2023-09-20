17e00e6829d67c1f5823bdc09c740cfcdb6e8977
ujjawalkumarsingh <singhujjawal9793@gmail.com>
2023-09-20 14:30:07 +0530

    Initial Commit

---
+++ Weather-SwiftUI/Weather_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_SwiftUIApp.swift
+//  Weather-SwiftUI
+//
+//  Created by Ujjawal Kumar Singh on 20/09/23.
+//
+
+import SwiftUI
+
+@main
+struct Weather_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
